56861df98c48499ef1778bb533abccc032312dfb
suudi abdisalan <abdisalansucdi@gmail.com>
2024-11-10 15:42:57 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+    <h1>Hi, I'm Suudi Abdisalan</h1>
+    <img alt="My profile pic" src="https://scontent.xx.fbcdn.net/v/t1.15752-9/462560810_1204940330615301_3538077835404816867_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=100&cb=04374bf5-a6759482&ccb=1-7&_nc_sid=0024fc&_nc_ohc=x7CXNRK8dyMQ7kNvgHEZOqQ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1QGt9K3eOBQV59c4aw-u6WF796eEymi9NPtgnZeF95tl0g&oe=67582209"/>
+    <h2>About Me</h2>
+    <p>
+    I am currently a student at Moringa School, where I am pursuing a Software Engineering program. <br></br>
+    At present, I am focusing on Frontend Development, building my skills in creating intuitive and dynamic user interfaces.<br></br> 
+    In the coming weeks, I will be transitioning to Backend Development to broaden my expertise and understanding of full-stack development.<br></br> 
+    I am passionate about coding, problem-solving, and continuously improving my technical abilities as I progress through my studies.
+    </p>
+    <nav>
+      <a href="https://github.com/Suudi-sudo" >GitHub</a>
+      
+      <br></br>
+
+    </nav>
+  </div>;
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,63 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+
+test("displays a top-level heading with the text `Hi, I'm Suudi Abdisalan`", () => {
+    render(<App />);
+  
+    const topLevelHeading = screen.getByRole("heading", {
+      name: /hi, i'm/i,
+      exact: false,
+      level: 1,
+    });
+  
+    expect(topLevelHeading).toBeInTheDocument();
+  });
+  
+  test("displays an image of yourself", () => {
+    render(<App />);
+  
+    const image = screen.getByAltText("My profile pic");
+  
+    expect(image).toHaveAttribute("src", "https://scontent.xx.fbcdn.net/v/t1.15752-9/462560810_1204940330615301_3538077835404816867_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=100&cb=04374bf5-a6759482&ccb=1-7&_nc_sid=0024fc&_nc_ohc=x7CXNRK8dyMQ7kNvgHEZOqQ&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1QGt9K3eOBQV59c4aw-u6WF796eEymi9NPtgnZeF95tl0g&oe=67582209");
+  });
+  
+  test("displays second-level heading with the text `About Me`", () => {
+    render(<App />);
+  
+    const secondLevelHeading = screen.getByRole("heading", {
+      name: /about me/i,
+      level: 2,
+    });
+  
+    expect(secondLevelHeading).toBeInTheDocument();
+  });
+  
+  test("displays a paragraph for your biography", () => {
+    render(<App />);
+  
+    const bio = screen.getByText(/I am current/i);
+  
+    expect(bio).toBeInTheDocument();
+  });
+  
+  test("displays the correct links", () => {
+    render(<App />);
+  
+    const githubLink = screen.getByRole("link", {
+      name: /GitHub/i,
+    });
+    // const linkedinLink = screen.getByRole("link", {
+    //   name: /linkedin/i,
+    // });
+  
+    expect(githubLink).toHaveAttribute(
+      "href",
+      expect.stringContaining("https://github.com/Suudi-sudo")
+    );
+  
+    // expect(linkedinLink).toHaveAttribute(
+    //   "href",
+    //   expect.stringContaining("https://linkedin.com")
+    // );
+  });
